94efc6cbe04bffab9a82c464452917b1b5ac3f20
Daniel Alvarez <dannymalvarez@gmail.com>
2021-11-18 10:57:46 -0500

    Following along with the course

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+var family = "The Alvarezes", Dad = "Daniel", Mom = "Stephanie", Dogter = "Sally";
+
+document.write(Dogter);
+document.write(Dad);
+document.write(Mom);
+document.write(family);
+
+var family = family.fontcolor("red");
+document.write(family);
+
+var Dad = Dad.fontcolor("pink");
+document.write(Dad);
+
+var Mom = Mom.fontcolor("green");
+document.write(Mom);
+var Dogter = Dogter.fontcolor("orange");
+document.write(Dogter);
+
+var B = (3 * 5)
+
+document.write(B);
