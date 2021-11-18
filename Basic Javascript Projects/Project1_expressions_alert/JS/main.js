3bb03e21e0150329f18093434c280c2437023ef5
Daniel Alvarez <dannymalvarez@gmail.com>
2021-11-18 11:08:19 -0500

    Added assignment requirements

--- Basic Javascript Projects/Project1_expressions_alert/JS/main.js
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -3,7 +3,7 @@ var family = "The Alvarezes", Dad = "Daniel", Mom = "Stephanie", Dogter = "Sally
 document.write(Dogter);
 document.write(Dad);
 document.write(Mom);
-document.write(family);
+window.alert(family);
 
 var family = family.fontcolor("red");
 document.write(family);
@@ -22,5 +22,9 @@ document.write(B);
 function My_First_Function() { // Defining a function and naming it
     var str = "This text is green!"; //Defining a variable and giving it a string value
     var result = str.fontcolor("green"); // Using the fontcolor method on str variable
-    document.getElementById("Green_Text").innerHTML = result; //putting the value of result into HTML element with "Green_Text" id
+    document.getElementById("Green_Text").innerHTML = result; //putting the value of result into HTML element with "Green_Text"
 }
+
+var C = "This is the first string that ";
+var D = "gets concatenated to the second string.";
+document.write(C + D);
