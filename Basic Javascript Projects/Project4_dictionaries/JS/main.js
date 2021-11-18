d5df48c2268881c26be5fd35d96f53cb8749cd4e
Daniel Alvarez <dannymalvarez@gmail.com>
2021-11-18 15:11:22 -0500

    added comments per instructions

--- Basic Javascript Projects/Project4_dictionaries/JS/main.js
+++ Basic Javascript Projects/Project4_dictionaries/JS/main.js
@@ -1,11 +1,11 @@
-function my_Dictionary() {
-    var Truck = {
-        Brand: "Dodge",
+function my_Dictionary() { // defining a function for a dictionary
+    var Truck = { // defining a variable named truck tht contains an object with key value pairs
+        Brand: "Dodge", // This is one of 4 key value pairs
         Model: "Ram 1500",
         Year: 2016,
         Color: "Blue"
     };
-    delete Truck.Model
-    document.getElementById("Dictionary").innerHTML = Truck.Model;
+    delete Truck.Model // this deletes one of the key value pairs
+    document.getElementById("Dictionary").innerHTML = Truck.Model; //this prints out the specified value to the html
 
 }
