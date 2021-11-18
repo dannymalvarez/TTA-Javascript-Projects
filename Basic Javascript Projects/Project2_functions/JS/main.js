3455c1dd74d503dfe9e4ae03b6136231a5832128
Daniel Alvarez <dannymalvarez@gmail.com>
2021-11-18 11:49:33 -0500

    more follow along practice

--- Basic Javascript Projects/Project2_functions/JS/main.js
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -1,3 +1,9 @@
 function displayDate() {
     document.getElementById("demo").innerHTML = Date();
 }
+
+function concat() { //defining my function to concatenate
+    var sentence = "I am learning"; // defining the sentence variable
+    sentence += " a lot from this book!"; // adding a second string to the sentence variable
+    document.getElementById("Concatenate").innerHTML = sentence; //putting the result "sentence" into the html by the concatenate id
+}
