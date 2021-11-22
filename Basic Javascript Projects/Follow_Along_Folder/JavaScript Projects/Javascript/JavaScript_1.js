74b4a97e4575eb80d43b581a7932e87ac464bfac
Daniel Alvarez <dannymalvarez@gmail.com>
2021-11-22 14:12:32 -0500

    New folder

---
+++ Basic Javascript Projects/Follow_Along_Folder/JavaScript Projects/Javascript/JavaScript_1.js
@@ -0,0 +1,12 @@
+function Color_Function() {
+    var Color_Output;
+    var Colors = document.getElementById("Color_Input").value;
+    var Color_String = " is a great color!";
+    switch(Colors) {
+        case "Red":
+            Color_Output = "Red" + Color_String;
+        break;
+        case "Yellow":
+            Color_Output = "Yellow" + Color_String;
+    }
+}
