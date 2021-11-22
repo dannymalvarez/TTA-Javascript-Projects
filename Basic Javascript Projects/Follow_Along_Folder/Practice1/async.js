1f83c1e7f8dd578674f9a6d92aad57df48abe04d
Daniel Alvarez <dannymalvarez@gmail.com>
2021-11-22 09:37:00 -0500

    Making folders and organizing them as I move along

---
+++ Basic Javascript Projects/Follow_Along_Folder/Practice1/async.js
@@ -0,0 +1,8 @@
+alert(document.getElementById("p1").innerHTML)
+
+function defer_Function() {
+    var A = 5;
+    var B = 17;
+    var addition = A + B;
+    document.getElementById("defer").innerHTML = addition;
+}
